cfd43b26ffa9a90fb8a17c50f9efaebdcc13932c
RaMirgayazov <RaMirgayazov@alabuga.ru>
2026-05-18 14:12:59 +0300

    refactor: project

--- skinprice/frontend/src/app/router/AppRouter.tsx
+++ skinprice/frontend/src/app/router/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { ROUTES } from "./routes";
 import { SavedSkinsPage } from "../../pages/saved-skins-page/ui/SavedSkinsPage";
 import { NewSkinsPage } from "../../pages/new-skins-page/ui/NewSkinsPage";
@@ -10,6 +10,5 @@ export const AppRouter: React.FC = () => (
     <Route path={ROUTES.home} element={<SavedSkinsPage />} />
     <Route path={ROUTES.newSkins} element={<NewSkinsPage />} />
     <Route path="*" element={<NotFoundPage />} />
-    <Route path="" element={<Navigate to={ROUTES.home} replace />} />
   </Routes>
 );
